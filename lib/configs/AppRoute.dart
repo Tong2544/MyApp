a457f8f715a9d25c1d7f5a351f98b618fbb4bdeb
Tong2544 <94897662+Tong2544@users.noreply.github.com>
2021-11-23 16:31:10 +0700

    Add files via upload

---
+++ lib/configs/AppRoute.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:mysql/configs/Type.dart';
+import 'package:mysql/configs/home.dart';
+import 'package:mysql/configs/login.dart';
+import 'package:mysql/configs/register.dart';
+
+
+
+class AppRoute {
+  static const homeRoute = 'home';
+  static const loginRoute = 'login';
+  static const registerRoute = 'register';
+  static const typeRoute = 'type';
+
+
+
+
+  final _route = <String, WidgetBuilder>{
+    homeRoute: (context) => HomeScreen(),
+    loginRoute: (context) => LoginScreen(),
+    registerRoute: (context) => RegisterScreen(),
+    typeRoute: (context) => TypeScreen(),
+
+  };
+
+  get getAll => _route;
+} //end class
